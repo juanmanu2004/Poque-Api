76d5a5f49f3e389b02d81cc0b72ac0c2f5b2ef7b
Juan Riola <130934364+juanmanu2004@users.noreply.github.com>
2025-05-09 19:53:55 +0000

    AvanzeFinalViernes/9

--- src/Componentes/Pokemon/index.jsx
+++ src/Componentes/Pokemon/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect,useContext } from 'react'
 import { useParams } from "react-router-dom"; 
 import { AppContext } from '../../Contexto/Contexto';
 import './style.css'
